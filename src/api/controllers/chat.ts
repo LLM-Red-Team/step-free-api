e9ed695b8d7062dfce464d9f39f7d22415125372
Yanyutin753 <132346501+Yanyutin753@users.noreply.github.com>
2024-04-01 22:48:34 +0800

    feat the context transfer files

--- src/api/controllers/chat.ts
+++ src/api/controllers/chat.ts
@@ -353,29 +353,26 @@ async function createCompletionStream(
  * @param messages 参考gpt系列消息格式，多轮对话请完整提供上下文
  */
 function extractRefFileUrls(messages: any[]) {
-  return messages.reduce((urls, message) => {
-    if (_.isArray(message.content)) {
-      message.content.forEach((v) => {
-        if (!_.isObject(v) || !["file", "image_url"].includes(v["type"]))
-          return;
-        // step-free-api支持格式
-        if (
-          v["type"] == "file" &&
-          _.isObject(v["file_url"]) &&
-          _.isString(v["file_url"]["url"])
-        )
-          urls.push(v["file_url"]["url"]);
-        // 兼容gpt-4-vision-preview API格式
-        else if (
-          v["type"] == "image_url" &&
-          _.isObject(v["image_url"]) &&
-          _.isString(v["image_url"]["url"])
-        )
-          urls.push(v["image_url"]["url"]);
-      });
-    }
+  const urls = [];
+  // 如果没有消息，则返回[]
+  if (!messages.length) {
     return urls;
-  }, []);
+  }
+  // 只获取最新的消息
+  const lastMessage = messages[messages.length - 1];
+  if (_.isArray(lastMessage.content)) {
+    lastMessage.content.forEach(v => {
+      if (!_.isObject(v) || !['file', 'image_url'].includes(v['type']))
+        return;
+      // step-free-api支持格式
+      if (v['type'] == 'file' && _.isObject(v['file_url']) && _.isString(v['file_url']['url']))
+        urls.push(v['file_url']['url']);
+      // 兼容gpt-4-vision-preview API格式
+      else if (v['type'] == 'image_url' && _.isObject(v['image_url']) && _.isString(v['image_url']['url']))
+        urls.push(v['image_url']['url']);
+    });
+  }
+  return urls;
 }
 
 /**
@@ -389,7 +386,13 @@ function extractRefFileUrls(messages: any[]) {
  * @param messages 参考gpt系列消息格式，多轮对话请完整提供上下文
  */
 function messagesPrepare(convId: string, messages: any[], refs: any[]) {
-  const content = messages.reduce((content, message) => {
+  let validMessages = messages.filter((message, index) => {
+    if (index === messages.length - 1) return true;
+    if (!Array.isArray(message.content)) return true;
+    // 不含"type": "image_url"或"type": "file"的消息保留
+    return !message.content.some(v => (typeof v === 'object' && ['file', 'image_url'].includes(v['type'])));
+  });
+  const content = validMessages.reduce((content, message) => {
     if (_.isArray(message.content)) {
       return message.content.reduce((_content, v) => {
         if (!_.isObject(v) || v["type"] != "text") return _content;
